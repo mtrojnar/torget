a4dda276effeea46de844e0e442c7ed28fdbe205
Michał Trojnara <Michal.Trojnara@stunnel.org>
2021-10-10 17:18:12 +0200

    Also build Darwin target

--- dist.sh
+++ dist.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p dist
-for OS in windows linux freebsd openbsd netbsd; do
+for OS in windows linux freebsd openbsd netbsd darwin; do
     echo "Building for $OS"
     GOOS=$OS GOARCH=amd64 go build -o "dist/torget-$OS" -ldflags '-w -s'
 done
